9af0622b606abf6baaa192e6da88d24fbe52306a
Alisher Kalkaman <Alisher Kalkaman>
2017-01-30 01:34:23 +0600

    wokr with files

---
+++ Lab2WorkWithFiles/Lab2WorkWithFiles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab2WorkWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader elements = new StreamReader(@"C:\Users\qalqa\Desktop\Numbers\Readme.txt");
+            string str = elements.ReadLine();
+            string[] arr = str.Split();
+
+            List<int> a = new List<int>();
+            foreach(string s in arr)
+            {
+                a.Add(int.Parse(s));
+            }
+
+            
+            
+          
+
+            
+
+            Console.ReadKey();
+         
+
+        }
+    }
+}
