95f20f2a89b1391c49f2a2a077c9d99092e3699e
Alisher Kalkaman <Alisher Kalkaman>
2017-01-30 23:57:25 +0600

    Working ReadFileMaxMin project

--- Lab2WorkWithFiles/Lab2WorkWithFiles/Program.cs
+++ Lab2WorkWithFiles/Lab2WorkWithFiles/Program.cs
@@ -3,29 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
+using System.IO;   //To work with directories
 
 namespace Lab2WorkWithFiles
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //numbers in file become => elements by "StreamReader" 
             StreamReader elements = new StreamReader(@"C:\Users\qalqa\Desktop\Numbers\Readme.txt");
             string str = elements.ReadLine();
             string[] arr = str.Split();
-
-            List<int> a = new List<int>();
-            foreach(string s in arr)
+            
+            // Way to convert "string massive" to "int massive"
+            int[] c = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
             {
-                a.Add(int.Parse(s));
+                c[i] = int.Parse(arr[i]) - 0;
             }
 
-            
-            
-          
+            //Last action is to find max and min of elements
+                int x = (-1000);
+                int y = 1000;
 
-            
+                for (int i=0; i<arr.Length; i++)
+            {
+                if (c[i] > x)
+                    x = c[i];
+                if (c[i] < y)
+                    y = c[i];
+            }
+
+
+
+            Console.WriteLine("Maximum is " + x);
+            Console.WriteLine("Minimum is " + y);
+
 
             Console.ReadKey();
          
