7cdb161f87697eebec134b05e358290d130877ef
Alisher Kalkaman <Alisher Kalkaman>
2017-03-08 14:26:15 +0600

    Snake Game with all modification

---
+++ SnakeGame/SnakeGame/Drawer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMySnake.Model
+{
+    [Serializable]
+    public class Drawer
+    {
+        public ConsoleColor color;
+        public List<Point> body = new List<Point>();
+        public char sign;
+
+        public Drawer() { }
+
+        public Drawer(ConsoleColor color, char sign, List<Point> body)
+        {
+            this.body = body;
+            this.color = color;
+            this.sign = sign;
+        }
+
+        public void Draw()
+        {
+            int i = 0;
+
+            Console.ForegroundColor = color;
+            
+            foreach (Point p in body)
+            {
+                Console.SetCursorPosition(p.x, p.y);
+                Console.Write(sign);
+                i++;
+            }
+        }
+
+
+        public void save()
+        {
+            Type t = this.GetType();
+            FileStream fs = new FileStream(String.Format("{0}.dat", t.Name), FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Close();
+        }
+
+        public void release()
+        {
+            
+            Console.Clear();
+
+            Type t = this.GetType();
+            FileStream fs = new FileStream(String.Format("{0}.dat", t.Name), FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            if (t == typeof(Wall))
+                Game.wall = bf.Deserialize(fs) as Wall;
+            if (t == typeof(Snake))
+                Game.snake = bf.Deserialize(fs) as Snake;
+            if (t == typeof(Food))
+                Game.food = bf.Deserialize(fs) as Food;
+
+
+
+
+            fs.Close();
+        }
+
+
+    }
+}
---
+++ SnakeGame/SnakeGame/Wall.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMySnake.Model
+{
+    [Serializable]
+    public class Wall : Drawer
+
+    {
+        public static int Level;
+        public Wall() { }
+
+        public Wall(ConsoleColor color, char sign, List<Point> body) : base(color, sign, body)
+        {
+            LoadLevel(1);
+        }
+
+        public void LoadLevel(int level)
+        {
+
+            // int cnt = Game.snake.body.Count;
+            Console.Clear();
+            body.Clear();
+
+
+            Level = level;
+           
+
+            string filename = string.Format(@"Levels\Level{0}.txt", level);
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string line = "";
+            int row = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                for (int col = 0; col < line.Length; col++)
+                {
+                    if (line[col] == '#')
+                    {
+                        body.Add(new Point(col, row));
+                    }
+                }
+                row++;
+            }
+
+
+        }
+
+
+        /*
+
+                public void save()
+                {
+                    FileStream fs = new FileStream("wall.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(fs, this);
+                    fs.Close();
+                }
+
+                public void deser()
+                {
+                    FileStream fs = new FileStream("wall.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    BinaryFormatter bf = new BinaryFormatter();
+                    Wall wall = bf.Deserialize(fs) as Wall;
+                    this.body = wall.body;
+                    this.color = wall.color;
+                    this.sign = wall.sign;
+
+
+                    fs.Close();
+                }
+            */
+    }
+}
