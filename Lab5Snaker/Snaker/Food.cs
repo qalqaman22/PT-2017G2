751de394a9882ce2adc009433561cf8e0f631254
qalqaman22 <qalqaman22@gmail.com>
2017-03-10 23:00:10 +0600

    SimpleSnake

---
+++ Lab5Snaker/Snaker/Food.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace MySnake
+{
+    [Serializable]
+    class Food
+    {
+        public Point location;
+        public ConsoleColor color;
+        public char sign;
+
+        public Food()
+        {
+            color = ConsoleColor.Green;
+            sign = '$';
+            SetRandomPosition();
+        }
+        public void Wallfood()
+        {
+            for (int i = 0; i < Game.wall.body.Count; i++)
+            {
+                if (location.x == Game.wall.body[i].x && location.y == Game.wall.body[i].y)
+                {
+                    SetRandomPosition();
+                }
+            }
+        }
+        public void SetRandomPosition()
+        {
+
+            int x = new Random().Next(0, 80);
+            int y = new Random().Next(0, 40);
+            // Is "x" and "y" on the wall or on the snake? If so, regenerate new position.
+
+            location = new Point(x, y);
+            Wallfood();
+        }
+
+        public void Draw()
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(location.x, location.y);
+            Console.Write(sign);
+        }
+        public void save()
+        {
+            FileStream fs = new FileStream("food.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Close();
+        }
+
+        public void deser()
+        {
+            FileStream fs = new FileStream("food.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            Food food = bf.Deserialize(fs) as Food;
+            this.location = food.location;
+            this.color = food.color;
+            this.sign = food.sign;
+
+
+            fs.Close();
+        }
+    }
+}
---
+++ Lab5Snaker/Snaker/Game.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MySnake
+{
+    [Serializable]
+    class Game
+    {
+        public static bool GameOver = false;
+        public static Snake snake = new Snake();
+        public static Wall wall = new Wall();
+        public static Food food = new Food();
+        
+
+        public static void Init()
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(20, 10);
+            Console.WriteLine("Press any key to start");
+            Console.SetCursorPosition(23, 11);
+            Console.WriteLine("...............");
+            Console.SetCursorPosition(25, 12);
+            Console.WriteLine("GOOD LUCK");
+            Console.ReadKey();
+            Console.CursorVisible = false;
+            Console.SetWindowSize(80, 40);
+        }
+        public static void Server()
+        {
+            while (!Game.GameOver)
+            {
+                Game.Draw();
+                ConsoleKeyInfo pressed = Console.ReadKey();
+
+                if (pressed.Key == ConsoleKey.UpArrow)
+                  Game.snake.Move(0, -1);
+                if (pressed.Key == ConsoleKey.DownArrow)
+                  Game.snake.Move(0, 1);
+                if (pressed.Key == ConsoleKey.LeftArrow)
+                  Game.snake.Move(-1, 0);
+                if (pressed.Key == ConsoleKey.RightArrow)
+                  Game.snake.Move(1, 0);
+
+                if (pressed.Key == ConsoleKey.F2)
+                    Game.Save();
+                if (pressed.Key == ConsoleKey.F3)
+                    Game.Resume();
+                if (pressed.Key == ConsoleKey.Escape)
+                    Game.GameOver = true;
+            }
+        }
+
+        public static void Draw()
+        {
+            Console.Clear();
+            snake.Draw();
+            wall.Draw();
+            food.Draw();
+        }
+
+        public static void Save()
+        {
+            Game.snake.save();
+            Game.wall.save();
+            Game.food.save();
+            // serialize all objects
+        }
+
+        public static void Resume()
+        {
+            Console.Clear();
+            Game.snake.deser();
+            Game.wall.deser();
+            Game.food.deser();
+            // deserialize all objects
+        }
+        public static void GameO()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(30, 15);
+            Console.WriteLine("GAME OVER!");
+            Console.SetCursorPosition(28, 16);
+            Console.WriteLine("NEVER GIVE UP!");
+            Console.ReadKey();
+        }
+    }
+}
